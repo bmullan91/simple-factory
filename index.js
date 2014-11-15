7b28204e08d54d3a48f74ec08bb8b233e3420760
Brian Mullan <bmullan91@gmail.com>
2014-11-15 23:11:16 +0000

    updates

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ module.exports = function(Class, validator) {
     throw new Error('simple-factory takes a function as it\'s first parameter.');
   }
 
+  if(validator && typeof validator !== 'function') {
+    throw new Error('simple-factory takes a function as it\'s second parameter');
+  }
+
   return function() {
     var args = Array.prototype.splice.call(arguments, 0);
-    //the first arg needs to be the scope - this
-    args.unshift(this);
 
     //pre-bind the arguments of the class to be what was passed
     //becuase you can't call .apply with the new keyword
-    var fn = Class.bind.apply(Class, args);
+    //NOTE: the first arg needs to be the scope - this
+    var fn = Class.bind.apply(Class, [this].concat(args));
 
     if(typeof validator === 'function') {
       return validator.apply(this, args) ? new fn() : null;
