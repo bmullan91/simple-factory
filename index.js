a036c8db215c90f91be2da95d9800a914d38565e
Brian Mullan <bmullan91@gmail.com>
2014-11-15 16:03:10 +0000

    updated to handle all args

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 module.exports = function(Class, validator) {
-  return function(data) {
+  if(typeof Class !== 'function') {
+    throw new Error('simple-factory takes a function as it\'s first parameter.');
+  }
+
+  return function() {
+    var args = Array.prototype.splice.call(arguments, 0);
+    //the first arg needs to be the scope - this
+    args.unshift(this);
+
+    //pre-bind the arguments of the class to be what was passed
+    //becuase you can't call .apply with the new keyword
+    var fn = Class.bind.apply(Class, args);
+
     if(typeof validator === 'function') {
-      return validator(data) ? new Class(data) : null;
+      return validator.apply(this, args) ? new fn() : null;
     } else {
-      return new Class(data);
+      return new fn();
     }
   };
 };
